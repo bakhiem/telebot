8d87e648611e9c04b10004d22d9bf64bfee70050
khiemnb <khiemnb2705@gmail.com>
2022-01-04 20:55:59 +0700

    change log

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ setInterval(() => {
   .get('https://api.bscscan.com/api?module=account&action=balance&address=0x02B1F22084ECE5962d4322c19ee0593EF345A14c&apikey=SDBDFW86Q225QFAJDYFVPIHPC4NENKCBU2')
   .then(response => {
     const balance = Number(response.data.result/1000000000000000000)
-    console.log('vao day bnbgo', balance)
     if(balance === oldBalanceGo) {
       return
     }
+    console.log('vao day bnbgo', balance)
     oldBalanceGo = balance
     if(balance > 0.5) {
       debounce_fun(balance.toFixed(3))
@@ -33,10 +33,10 @@ setInterval(() => {
   .get('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x5a14D961B2Cd0e69e7DE0EB0F5D4E77bad64654f&tag=latest&apikey=9WVEQZK29NFBP1HFMA88VUXSVSCWEVN86T')
   .then(response => {
     let balance = Number(response.data.result/1000000000000000000)
-    console.log('vao day bscevo', balance)
     if(balance === oldBalanceMatrix) {
       return
     }
+    console.log('vao day bscevo', balance)
     oldBalanceMatrix = balance
     if(balance > 0.05) {
       debounce_funMatrix(balance.toFixed(3))
